f63877e6d801d418c8bc77a89a4f3ae3fc4206a9
KirbyClaro <kirbychristianclaro@gmail.com>
2026-01-03 20:01:34 +0800

    added text and started while statement

--- index.py
+++ index.py
@@ -2,3 +2,9 @@ from datetime import datetime
 
 def parking_garage():
     garage = []
+    
+    print("--- LIFO Parking Garage Simulation ---")
+    print("System initialized. Garage is empty.")
+    
+    while True:
+        
