bc7dcfbfacfe2abe71a0013ff9dab351cdc6ce1c
KirbyClaro <kirbychristianclaro@gmail.com>
2026-01-11 20:47:19 +0800

    added class GarageApp

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime
 
+class GarageApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("LIFO Parking Garage System")
+        self.root.geometry("500x600")
+        
